5dacd7f7f3f2872426226759fccd2968f4b1ecac
Wilder León <wilder_eduardo85@hotmail.com>
2021-07-03 15:27:41 -0400

    logs reducer types and actions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 
 const App = ()=> {
