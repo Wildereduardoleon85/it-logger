287b17fe556c0e5b47ba957b3aab6e59529f6bee
Wilder León <wilder_eduardo85@hotmail.com>
2021-07-02 23:19:37 -0400

    add btn and addlogmodal component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Searchbar from './components/layout/Searchbar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = ()=> {
 
@@ -16,6 +18,8 @@ const App = ()=> {
     <Fragment>
       <Searchbar/>
       <div className="container">
+        <AddBtn/>
+        <AddLogModal/>
         <Logs/>
       </div>
     </Fragment>
